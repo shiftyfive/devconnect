016973bc3f02381b2d75f59e2f0fdc4682425e44
Joshua <joshua@JW-dev.local>
2017-04-24 11:48:02 -0700

    added information to seed files

--- db/seeds/1users.js
+++ db/seeds/1users.js
@@ -10,4 +10,48 @@ exports.seed = knex => knex('users').del()
         email: 'AmericanHockeyJesus@gmail.com',
         hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
       },
+      {
+        id: 1,
+        first_name: 'Austin',
+        last_name: 'Mathews',
+        email: 'AmericanHockeyJesus@gmail.com',
+        hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+      },
+      {
+        first_name: 'April',
+        last_name: 'Ludgate',
+        email: 'april.l@parksandrec.gov',
+        hashed_password: '',
+      },
+      {
+        first_name: 'Jeremy',
+        last_name: 'Jacobs',
+        email: 'twofirstnames@hotmail.com',
+        hashed_password: '',
+      },
+      {
+        first_name: 'Corrin',
+        last_name: 'Lynn',
+        email: 'flynn@gmail.com',
+        hashed_password: '',
+      },
+      {
+        first_name: 'Gregory',
+        last_name: 'Hanzel',
+        email: 'cvs2@gmail.com',
+        hashed_password: '',
+      },
+      {
+        first_name: 'Cynthia',
+        last_name: 'Bozak',
+        email: 'bozak.c@gmail.com',
+        hashed_password: '',
+      },
+      {
+        first_name: 'Dwight',
+        last_name: 'Schrute',
+        email: 'd.schrute@dmifflin',
+        hashed_password: '',
+      },
+
     ]));

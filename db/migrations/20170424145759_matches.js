93740e0a80ac7a128083948a3fd7e59dac840004
Joshua <joshuawarren000@gmail.coml>
2017-04-24 15:39:13 -0700

    merge issues

--- db/migrations/20170424145759_matches.js
+++ db/migrations/20170424145759_matches.js
@@ -1,7 +1,7 @@
 exports.up = (knex) => {
   return knex.schema.createTable('matches', (table) => {
     table.increments();
-    table.integer('user_id1').references('users.id')
+    table.integer('user_id1').references('users.id');
     table.integer('user_id2').references('users.id');
     table.bool('is_matched');
   });

40253b20546fb1800dc9b602f3fc09e52066fc8a
Joshua <joshuawarren000@gmail.coml>
2017-04-24 15:49:05 -0700

    try to fix life

--- db/seeds/1users.js
+++ db/seeds/1users.js
@@ -6,6 +6,7 @@ exports.seed = knex => knex('users').del()
         id: 1,
         first_name: 'Austin',
         last_name: 'Mathews',
+        user_name: 'Lonedawg007',
         email: 'AmericanHockeyJesus@gmail.com',
         hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
       },

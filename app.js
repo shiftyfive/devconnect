91ab09dbed4f95bccd5cfc1136bb2963c61eaee2
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-24 09:29:37 -0700

    Added some in app.js

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict'
+
+if (process.env.NODE_ENV !== 'production' && !process.env.IS_BUILD) {
+  require('dotenv').config()
+}
+
+app.disable('x-powered-by');
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const morgan = require('morgan');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,13 +37,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.enable('trust proxy');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(cookieSession({
+  name: 'bookshelf',
+  secret: process.env.SESSION_SECRET,
+  secure: app.get('env') === 'production'
+}));
 
 app.use('/', index);
 app.use('/users', users);

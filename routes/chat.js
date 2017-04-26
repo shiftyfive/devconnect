014157b3c8c544e800d9b38ed050aea340776e5d
Joshua <joshuawarren000@gmail.coml>
2017-04-26 09:54:10 -0700

    added additional code for chat messages to display

--- routes/chat.js
+++ routes/chat.js
@@ -21,28 +21,11 @@ io.on('connection', (socket) => {
   });
 });
 
-router.get('/', (req, res) => {
-  res.render('chats');
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    console.log(`message: ${msg}`);
+    io.emit('chat message', msg);
   });
 });
-
-
-app.get('/users/:id/chat') {
-  res.sendFile(__diname + '/')
-}
-
-
 
 module.exports = router;
